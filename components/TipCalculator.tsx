080a99806fdc27f9520959d1d4a3c1f9696fe77f
shalri <fsaonlinework777@gmail.com>
2024-06-07 23:55:27 +0800

    Mobile design layout

--- components/TipCalculator.tsx
+++ components/TipCalculator.tsx
@@ -1,20 +1,22 @@
 "use client";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
 
 export default function TipCalculator() {
-  const [bill, setBill] = useState<number>(0);
+  const [bill, setBill] = useState<number | null>(0);
   const [tipPercentage, setTipPercentage] = useState<number>(0);
-  const [numberOfPeople, setNumberOfPeople] = useState<number | null>(null);
+  const [numberOfPeople, setNumberOfPeople] = useState<number | null>(0);
   const [customTip, setCustomTip] = useState<string | number>("");
   const [tip, setTip] = useState<number>(0);
   const [total, setTotal] = useState<number>(0);
 
   const calculateTip = useCallback(() => {
     if (numberOfPeople && numberOfPeople > 0) {
-      const tipAmount = (bill * (tipPercentage / 100)) / numberOfPeople;
+      const billAmount = bill ?? 0;
+      const tipAmount = (billAmount * (tipPercentage / 100)) / numberOfPeople;
       const totalAmount =
-        (bill + bill * (tipPercentage / 100)) / numberOfPeople;
+        (billAmount + billAmount * (tipPercentage / 100)) / numberOfPeople;
       setTip(tipAmount);
       setTotal(totalAmount);
     }
@@ -27,7 +29,10 @@ export default function TipCalculator() {
   }, [bill, tipPercentage, numberOfPeople, calculateTip]);
 
   const handleBillChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBill(parseFloat(e.target.value) || 0);
+    const newBill = e.target.value;
+    if (newBill === "" || parseInt(newBill) > 0) {
+      setBill(newBill === "" ? null : parseInt(newBill));
+    }
   };
 
   const handleTipPercentageChange = (percentage: number) => {
@@ -76,26 +81,31 @@ export default function TipCalculator() {
           />
         </h1>
       </header>
-      <div className=" bg-tc-white">
-        <section className="px-8 py-10">
+      <div className="rounded-t-3xl bg-tc-white">
+        <section className="px-8 pt-10">
           <form onSubmit={(e) => e.preventDefault}>
             <div className="flex flex-col">
               <label
                 htmlFor="bill"
-                className="text-[1rem] text-tc-grayish-cyan"
+                className="mb-2 text-[1rem] text-tc-grayish-cyan"
               >
                 Bill
               </label>
               <input
                 type="number"
-                value={bill}
+                value={bill || ""}
+                placeholder="0"
                 onChange={handleBillChange}
-                className="bg-tc-very-light-grayish"
+                // className="form-input border-2 border-tc-white bg-tc-very-light-grayish text-right focus:border-2 focus:border-tc-strong-cyan focus:ring-0 focus:ring-offset-0 active:border-2 active:ring-tc-strong-cyan"
+                className="input-tc text-tc-very-dark-cyan"
                 id="bill"
               />
             </div>
-            <div className="grid grid-cols-2">
-              <label htmlFor="tip" className="col-span-2">
+            <div className="mt-9 grid grid-cols-2 gap-4">
+              <label
+                htmlFor="tip"
+                className="col-span-2 text-[1rem] text-tc-grayish-cyan"
+              >
                 Select Tip %
               </label>
               {[5, 10, 15, 25, 50].map((percentage) => (
@@ -104,33 +114,47 @@ export default function TipCalculator() {
                   id="tip"
                   key={percentage}
                   onClick={() => handleTipPercentageChange(percentage)}
+                  className={cn(
+                    "h-12 rounded-lg bg-tc-very-dark-cyan text-tc-white transition-colors duration-300",
+                    tipPercentage === percentage
+                      ? "bg-tc-strong-cyan text-tc-very-dark-cyan"
+                      : "",
+                  )}
                 >
                   {percentage}%
                 </button>
               ))}
               <input
-                type="text"
+                type="number"
                 id="custom"
                 placeholder="Custom"
                 // value={customTip}
                 value={customTip === 0 ? "Custom" : customTip}
                 onChange={handleCustomTipChange}
-                className="rounded border"
+                // className="form-input rounded border"
+                className="input-tc text-tc-very-dark-cyan"
               />
             </div>
             <div className="flex flex-col">
-              <label htmlFor="number-of-people">Number of People</label>
+              <label
+                htmlFor="number-of-people"
+                className="mb-2 mt-9 text-[1rem] text-tc-grayish-cyan"
+              >
+                Number of People
+              </label>
               <input
-                type="text"
+                type="number"
                 id="number-of-people"
                 value={numberOfPeople || ""}
+                placeholder="0"
                 onChange={handleNumberOfPeopleChange}
-                className="rounded border"
+                // TODO: change scheme when bill is > 0 and number is === 0
+                className="input-tc text-tc-very-dark-cyan"
               />
             </div>
           </form>
         </section>
-        <section className="w-full bg-tc-very-dark-cyan px-9 py-11">
+        <section className="mb mx-6 my-8 rounded-2xl bg-tc-very-dark-cyan px-6 py-11">
           <div>
             <h2 className="flex w-full justify-between">
               <div className="block">Tip Amount</div>
